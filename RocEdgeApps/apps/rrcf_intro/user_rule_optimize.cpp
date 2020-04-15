5bca5e3849f6434d2b36708c22f2bf7949a9c670
Satoshi Shimamura <53499005+roconics@users.noreply.github.com>
2020-04-15 20:01:09 +0900

    Create user_rule_optimize.cpp

---
+++ RocEdgeApps/apps/rrcf_intro/user_rule_optimize.cpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright 2019, 2020 - , Roconics Co., All Rights Reserved.
+ */
+
+#include "user_rule.hpp"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+namespace urule {
+
+
+	int UserRule::userRuleOptomizationSetup()
+	{
+		// multi-thread user_rules
+		// initialize annex memory db
+		// the ledger of trading is MIN1 db
+
+		if (simulation) {
+
+		}
+
+		return 0;
+	}
+
+}
