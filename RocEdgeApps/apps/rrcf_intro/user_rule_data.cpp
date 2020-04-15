52fcb6b5726912a2ea2fc3f80adad1e4c2f11463
Satoshi Shimamura <53499005+roconics@users.noreply.github.com>
2020-04-15 20:01:01 +0900

    Create user_rule_data.cpp

---
+++ RocEdgeApps/apps/rrcf_intro/user_rule_data.cpp
@@ -0,0 +1,24 @@
+/*
+ * Copyright 2019,2020-, Roconics Co., All Rights Reserved.
+ */
+
+#include "user_rule.hpp"
+
+#
+
+
+namespace urule {
+
+	void UserRule::endOfSimulatorRun()
+	{
+		int ex = roc::DUKAS;
+		int in = roc::EURUSD;
+		int tf = roc::TICK;
+
+		if (annex_only)return;
+
+
+	}
+
+
+}
